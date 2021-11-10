753fa73fdfdded356548a41493a81c9432fe59f0
warrensinclair <92188682+warrensinclair@users.noreply.github.com>
2021-11-10 08:43:18 +0300

    shows all NFT and tokens on Solana
    Phantom and Sollet wallet are connnected

---
+++ src/components/TokenItem.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { formatPrice, fromLamports } from '$lib/number';
+	import type { TokenInfo, TokenMap } from '$lib/tokens';
+
+	export let account: TokenInfo;
+	export let tokenMap: Promise<TokenMap>;
+
+	$: tokenInfo = undefined;
+
+	onMount(async () => {
+		tokenMap.then((tokens) => {
+			tokenInfo = tokens.get(account.mint.toBase58());
+		});
+	});
+</script>
+
+{#if tokenInfo}
+	<div class="flex p-2 space-x-3 hover:bg-purple-900 rounded cursor-pointer">
+		<div class="">
+			<img class="rounded-full h-12 w-12" src={tokenInfo.logoURI} alt="" height="50px" />
+		</div>
+
+		<div class="flex justify-between w-full">
+			<div>
+				<div class="font-bold">{tokenInfo.symbol}</div>
+				<div class="text-xs text-gray-400">{tokenInfo.name}</div>
+			</div>
+
+			<div class="text-gray-400">
+				{formatPrice(fromLamports(account.amount, tokenInfo.decimals))}
+				{tokenInfo.symbol}
+			</div>
+		</div>
+	</div>
+{:else}
+	Unknown Token: {account.mint.toBase58()}
+{/if}
---
+++ src/routes/index.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import TokenList from '../components/TokenList.svelte';
+	import ConnectButton from '../components/ConnectButton.svelte';
+	import SolBalance from '../components/SolBalance.svelte';
+	import { pubKey } from '../stores/signer';
+</script>
+
+<svelte:head>
+	<script>
+		global = globalThis; // for solana web3 repo
+	</script>
+</svelte:head>
+
+<main>
+	<div class="flex justify-center">
+		<div class="flex justify-center my-4" style="min-width: 440px;">
+			<ConnectButton />
+		</div>
+	</div>
+
+	{#if $pubKey}
+		<div class="flex justify-center">
+			<div style="min-width: 440px;">
+				<SolBalance />
+				<TokenList />
+			</div>
+		</div>
+	{/if}
+</main>
+
+<style type="text/css">
+	:root {
+		background-color: rgb(22, 0, 41);
+	}
+</style>
